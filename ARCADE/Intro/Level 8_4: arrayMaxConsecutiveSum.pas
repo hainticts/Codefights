633d9916ccb7e75c84ca738229e4f764cee4545c
tranquangtanqt <tranquangtanqt1990@gmail.com>
2017-08-17 22:34:13 +0700

    Create Level 8_4: arrayMaxConsecutiveSum.pas

---
+++ ARCADE/Intro/Level 8_4: arrayMaxConsecutiveSum.pas
@@ -0,0 +1,29 @@
+program ideone;
+
+Type
+	ArrayInteger = Array of integer;
+
+function arrayMaxConsecutiveSum(inputArray: ArrayInteger; k: integer): integer;
+var i, j, sum, max : integer;
+begin
+    max := 0; sum := 0;
+    for i := 0 to k - 2 do
+        sum := sum + inputArray[i];
+        
+    for i := k - 1 to length(inputArray) - 1 do
+    begin
+        sum := sum + inputArray[i];
+        if(sum > max) then max := sum;
+        sum := sum - inputArray[i-k+1];
+    end;
+    
+    arrayMaxConsecutiveSum := max;
+end;
+
+var inputArray : ArrayInteger;
+	k : integer;
+begin
+	k := 2;
+	inputArray := ArrayInteger.Create(2, 3, 5, 1, 6);
+	writeln(arrayMaxConsecutiveSum(inputArray, k));
+end.
