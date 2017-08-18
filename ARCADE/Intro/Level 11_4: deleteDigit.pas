d6d93d0b5ec3f8285f27e5e9389f0b37a2c018a0
tranquangtanqt <tranquangtanqt1990@gmail.com>
2017-08-19 00:01:28 +0700

    Update Level 11_4: deleteDigit.pas

--- ARCADE/Intro/Level 11_4: deleteDigit.pas
+++ ARCADE/Intro/Level 11_4: deleteDigit.pas
@@ -1,1 +1,29 @@
-a
+program deleteDigit;
+uses Sysutils;
+
+function deleteDigit(n: integer): integer;
+var s, s1 : string;
+    i, max, a : integer;
+begin
+    max := 0; s := '';
+    s := IntToStr(n);
+    
+    for i := 1 to length(s) do
+    begin
+        s1 := s[i];
+        Delete(s, i, 1);
+        a := StrToInt(s);
+        Insert(s1, s, i);
+        if(a > max) then max := a;
+    end;
+    
+    deleteDigit := max;
+end;
+
+var n : integer;
+begin
+	n := 125;
+	writeln(deleteDigit(n));
+	n := 1001;
+	writeln(deleteDigit(n));
+end.
