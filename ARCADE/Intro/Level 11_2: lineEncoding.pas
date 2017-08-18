8195439870eb46b51e5c6ffb6895560a3bc44aa9
tranquangtanqt <tranquangtanqt1990@gmail.com>
2017-08-18 21:10:17 +0700

    Create Level 11_2: lineEncoding.pas

---
+++ ARCADE/Intro/Level 11_2: lineEncoding.pas
@@ -0,0 +1,22 @@
+function lineEncoding(s: string): string;
+var i, j, count : integer;
+    rs : string;
+begin
+    i := 1; rs := '';
+    while(i <= length(s)) do
+    begin
+        count := 1;
+        j := i;
+        while(j < length(s)) do
+        begin
+            j := j + 1;
+            if(s[i] = s[j]) then inc(count)
+            else break;
+        end;
+        if(count <> 1) then rs := rs + IntToStr(count) + s[i]
+        else rs := rs + s[i];
+        inc(i, count);
+    end;
+    
+    lineEncoding := rs;
+end;
