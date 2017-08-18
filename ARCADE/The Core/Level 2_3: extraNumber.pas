a0100f89816820bc42b008b743ddf3fb726cf142
tranquangtanqt <tranquangtanqt1990@gmail.com>
2017-08-19 03:37:01 +0700

    Create Level 2_3: extraNumber.pas

---
+++ ARCADE/The Core/Level 2_3: extraNumber.pas
@@ -0,0 +1,17 @@
+program codechef;
+
+function extraNumber(a: integer; b: integer; c: integer): integer;
+var rs : integer;
+begin
+    rs := 0;
+    if(a = b) then rs := c;
+    if(b = c) then rs := a;
+    if(a = c) then rs := b;
+    extraNumber := rs;
+end;
+
+var a, b, c : integer;
+begin
+	a := 2; b := 7; c := 2;
+	writeln(extraNumber(a, b, c));
+end.
