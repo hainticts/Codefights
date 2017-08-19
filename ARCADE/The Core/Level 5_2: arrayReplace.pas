39f4bf59285b4ff88f88ef2f20defc21fae0d241
tranquangtanqt <tranquangtanqt1990@gmail.com>
2017-08-19 23:04:08 +0700

    Update Level 5_2: arrayReplace.pas

--- ARCADE/The Core/Level 5_2: arrayReplace.pas
+++ ARCADE/The Core/Level 5_2: arrayReplace.pas
@@ -1,22 +1,20 @@
-program createArray;
+program arrayReplace;
 Type
     ArrayInteger = Array of integer;
-function createArray(size: integer): ArrayInteger;
-var A : ArrayInteger;
-    i : integer;
+
+function arrayReplace(inputArray: ArrayInteger; elemToReplace: integer; substitutionElem: integer): ArrayInteger;
+var i :  integer;
 begin
-    SetLength(A, size);
-    for i := 0 to length(A) - 1 do
-        A[i] := 1;
-    
-    createArray := A;
+    for i := 0 to length(inputArray) - 1 do
+        if(inputArray[i] = elemToReplace) then inputArray[i] := substitutionElem;
 end;
 
-var size, i : integer;
+var i, elemToReplace, substitutionElem : integer;
     A : ArrayInteger;
 begin
-	size := 4;
-	A := createArray(size);
-	for i:= 0 to length(A) - 1 do
+	A := ArrayInteger.Create(1, 2, 1);
+	elemToReplace := 1; substitutionElem := 3;
+	A := arrayReplace(A, elemToReplace, substitutionElem);
+	for i := 0 to length(A) - 1 do
 	    write(A[i], ' ');
 end.
