8d2daa2eb2450bda8aa8817ce0a26f2f5b4939d0
tranquangtanqt <tranquangtanqt1990@gmail.com>
2017-08-19 17:44:29 +0700

    Update Level 3_5: secondRightmostZeroBit.cs

--- ARCADE/The Core/Level 3_5: secondRightmostZeroBit.cs
+++ ARCADE/The Core/Level 3_5: secondRightmostZeroBit.cs
@@ -2,9 +2,21 @@
      
     public class Test
     {
-    	public static int secondRightmostZeroBit(int n){
-                return -~((n - ~(n ^ (n + 1)) / 2) ^ (n - ~(n ^ (n + 1)) / 2 + 1)) / 2;
-            }
+    	public static int secondRightmostZeroBit(int n)
+        {
+            return new Func<int>(() => 
+                   {
+                       string s = Convert.ToString(n, 2);
+                       int j = -1, count = 0;
+                       for(int i = s.Length - 1; i >= 0; i--){
+                           j++;
+                           if(s[i] == '0') count++;
+                           if(count == 2) break;
+
+                        }
+                     return (int)Math.Pow(2, j);
+                   })() ;
+        }
     	public static void Main()
     	{
     		int n = 37;
