b53f146eeca46b1117efe0f2e78da28f24f012ff
tranquangtanqt <tranquangtanqt1990@gmail.com>
2017-08-19 18:08:37 +0700

    Update Level 3_6: swapAdjacentBits.cs

--- ARCADE/The Core/Level 3_6: swapAdjacentBits.cs
+++ ARCADE/The Core/Level 3_6: swapAdjacentBits.cs
@@ -1,13 +1,26 @@
-    using System;
+using System;
      
     public class Test
     {
-    	public static int swapAdjacentBits(int n){
-            return (((n & 0x2AAAAAAA) >> 1) | ((n & 0x15555555) << 1));
-        }
+    	public static int swapAdjacentBits(int n)
+		{
+		  return new Func<int>(() => 
+		                 {
+		                    string s = Convert.ToString(n, 2);
+		                    if(s.Length % 2 == 1) s = "0" + s;
+		                    string rs = "";
+		                    for(int i = 0; i < s.Length; i += 2){
+		                       rs += s[i + 1];
+		                       rs += s[i];
+		                    }
+		                    return Convert.ToInt32(rs, 2);
+		                 })() ;
+		}
     	public static void Main()
     	{
     		int n = 13;
+    		Console.WriteLine(swapAdjacentBits(n));
+    		n = 74;
     		Console.Write(swapAdjacentBits(n));
     	}
     }
