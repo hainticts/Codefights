2602ceeb5520e37ee9856c75a026a778d4ae72af
tranquangtanqt <tranquangtanqt1990@gmail.com>
2017-08-19 14:24:58 +0700

    Update Level 3_2: arrayPacking.cs

--- ARCADE/The Core/Level 3_2: arrayPacking.cs
+++ ARCADE/The Core/Level 3_2: arrayPacking.cs
@@ -1,27 +1,17 @@
     using System;
-     
-    public class Test
-    {
-    	public static int arrayPacking(int[] a){
-            if (1 <= a.Length && a.Length <= 4){
-                int result = 0;
-                for (int index = 0; index < a.Length; index++){
-                    if (a[index] > 256 || a[index] < 0){
-                        throw new ArgumentOutOfRangeException();
-                    }
-                    else{
-                        result += a[index] << 8 * index;
-                    }
-                }
-                return result;
-            }
-            else{
-                throw new ArgumentOutOfRangeException();
-            }
-        }
-    	public static void Main()
-    	{
-    		int []  a = new int []{24, 85, 0};
-    		Console.Write(arrayPacking(a));
-    	}
-    }
+ 
+public class Test
+{
+	public static int arrayPacking(int[] a) {
+	    int rs = 0;
+	    for(int i = 0; i < a.Length; i++){
+	        rs += a[i] << (8 * i);
+	    }
+	    return rs;
+	} 
+	public static void Main()
+	{
+		int [] a = new int []{24, 85, 0};
+		Console.WriteLine(arrayPacking(a));
+	}
+}
