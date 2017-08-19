73e12718014b56eca5682242b0ff65f89faa9290
tranquangtanqt <tranquangtanqt1990@gmail.com>
2017-08-19 16:22:46 +0700

    Create Level 3_4: mirrorBits.pas

---
+++ ARCADE/The Core/Level 3_4: mirrorBits.pas
@@ -0,0 +1,21 @@
+program codechef;
+
+function mirrorBits(a: integer): integer;
+var rs : integer;   
+begin
+    rs := 0;
+    while(a > 0) do
+    begin
+        rs := rs * 2 + (a mod 2);
+        a := a div 2;
+    end;
+    mirrorBits:= rs;
+end;
+
+var a : integer;
+begin
+	a := 97;
+	writeln(mirrorBits(a));
+	a := 8;
+	writeln(mirrorBits(a));
+end.
