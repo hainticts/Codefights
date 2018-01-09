413dc52c326a27826a77dfc04944e33f09ba5b00
tranquangtanqt <tranquangtanqt1990@gmail.com>
2018-01-09 16:54:40 +0700

    Update Level 1_3: checkPalindrome.cs

--- ARCADE/Intro/Level 1_3: checkPalindrome.cs
+++ ARCADE/Intro/Level 1_3: checkPalindrome.cs
@@ -2,22 +2,22 @@ using System;
 
 public class Test
 {
-	public static bool checkPalindrome(string inputString){
-        string inputString1 = "";
-        for (int i = inputString.Length - 1; i >= 0; i--){
-        	inputString1 = inputString1 + inputString[i];
+ public static bool checkPalindrome(string inputString) {
+    for(int i = 0; i < inputString.Length / 2; i++){
+        if(inputString[i] != inputString[inputString.Length - i - 1]){
+            return false;
         }
-        if (String.Compare(inputString, inputString1, true) == 0) return true;
-        return false;
     }
+    return true;
+}
     
-	public static void Main()
-	{
-		string inputString = "aabaa";
-		Console.WriteLine(checkPalindrome(inputString));
-		string inputString1 = "abac";
-		Console.WriteLine(checkPalindrome(inputString1));
-		string inputString2 = "a";
-		Console.WriteLine(checkPalindrome(inputString2));
-	}
+ public static void Main()
+ {
+  string inputString = "aabaa";
+  Console.WriteLine(checkPalindrome(inputString));
+  string inputString1 = "abac";
+  Console.WriteLine(checkPalindrome(inputString1));
+  string inputString2 = "a";
+  Console.WriteLine(checkPalindrome(inputString2));
+ }
 }
